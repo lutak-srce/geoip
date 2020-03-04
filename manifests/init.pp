7beba55c08409cdb63a9d4efceb2c0dcf1f84180
Jakov Sosic <jsosic@gmail.com>
2020-03-04 14:00:22 +0100

    Rework the module for Max Mind changes
    MaxMind changed licenceses for GeoIPs, so old way of using it doesn't
    work. These changes follow geoipupdate v3.1.1, which needs to be custom
    built and installed on CentOS

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,62 +1,10 @@
-# Class: pyqt4
 #
-# This module manages PyQt4 toolkit
+# = Class: geoip
 #
-class geoip (
-  $package_ensure = $geoip::params::package_ensure,
-  $geoip_version  = $geoip::params::geoip_version,
-) inherits geoip::params {
-  require perl::mod::libwww
+# This module manages geoip
+#
+class geoip {
 
-  # install specific version
-  package { 'GeoIP':
-    ensure => $package_ensure,
-  }
+  package { 'GeoIP': }
 
-  File {
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    require => Package['GeoIP'],
-  }
-
-  # package brings directory, but we decide to purge it
-  file { '/usr/share/GeoIP':
-    ensure  => directory,
-    purge   => true,
-    mode    => '0755',
-  }
-
-  # fetch and manage GeoIP.dat
-  #  exec { 'fetch_geoip':
-  #  command => '/bin/touch /usr/share/GeoIP/GeoIP.dat && /usr/bin/perl /usr/share/doc/GeoIP-*/fetch-geoipdata.pl > /dev/null 2>&1',
-  #  unless  => '/usr/bin/test -s /usr/share/GeoIP/GeoIP.dat',
-  #  require => File['/usr/share/GeoIP'],
-  #}
-  #file { '/usr/share/GeoIP/GeoIP.dat':
-  #  require => Exec['fetch_geoip'],
-  #}
-
-  # fetch and manage GeoLiteCity.dat
-  exec { 'fetch_geolitecity':
-    command => '/bin/touch /usr/share/GeoIP/GeoLiteCity.dat && /usr/bin/perl /usr/share/doc/GeoIP-*/fetch-geoipdata-city.pl > /dev/null 2>&1',
-    unless  => '/usr/bin/test -s /usr/share/GeoIP/GeoLiteCity.dat',
-    require => File['/usr/share/GeoIP'],
-  }
-  file { '/usr/share/GeoIP/GeoLiteCity.dat':
-    require => Exec['fetch_geolitecity'],
-  }
-
-  file { '/usr/share/GeoIP/GeoIPCity.dat':
-    ensure  => symlink,
-    target  => 'GeoLiteCity.dat',
-    require => File['/usr/share/GeoIP/GeoLiteCity.dat'],
-  }
-
-  # files are fetched via cron every month
-  #file { '/etc/cron.monthly/fetch-geodata':
-  #  mode    => '0755',
-  #  content => template('geoip/fetch-geodata.erb'),
-  #}
 }
--- manifests/update.pp
+++ manifests/update.pp
@@ -4,42 +4,31 @@
 # This module manages GeoIP update script
 #
 class geoip::update (
-  $package            = $::geoip::params::package_update,
-  $package_ensure     = $::geoip::params::package_ensure,
-  $geoipconf_path     = $::geoip::params::geoipconf_path,
-  $geoipconf_template = $::geoip::params::geoipconf_template,
-  $geoipupdate_cron   = 'geoip/geoipupdate.cron.erb',
-  $geoipupdate_time   = '6 5 * * 4',
-  $userid             = '999999',
+  $package            = 'geoipupdate',
+  $package_ensure     = 'present',
+  $geoipconf_template = 'geoip/GeoIP.conf.erb',
+  $account_id         = '0',
   $license_key        = '000000000000',
-  $product_ids        = 'GeoLite2-Country',
-) inherits geoip::params {
+  $edition_ids        = 'GeoLite2-Country GeoLite2-City',
+) {
 
-  package { 'GeoIP-update':
+  package { 'geoipupdate':
     ensure => $package_ensure,
     name   => $package,
   }
 
-  File {
+  file { '/etc/GeoIP.conf':
     ensure  => file,
     owner   => root,
     group   => root,
     mode    => '0644',
-    require => Package['GeoIP-update'],
-  }
-
-  file { '/etc/GeoIP.conf':
-    path    => $geoipconf_path,
     content => template($geoipconf_template),
-    notify  => Exec['geoip_update_initial'],
+    require => Package['geoipupdate'],
+    notify  => Exec['geoipupdate_initial'],
   }
 
-  file { '/etc/cron.d/geoipupdate':
-    content => template($geoipupdate_cron),
-  }
-
-  exec { 'geoip_update_initial':
-    command     => '/usr/bin/geoipupdate',
+  exec { 'geoipupdate_initial':
+    command     => '/usr/bin/geoipupdate -v',
     refreshonly => true,
   }
 
